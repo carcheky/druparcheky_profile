51686934fe051c4bc3d38c923a810739bc44d5fd
carcheky <carcheky@gmail.com>
2019-09-30 16:40:26 +0200

    split core and contrib

--- testing-dev.sh
+++ testing-dev.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# AUTOINSTALACIÓN ES
+# AUTOINSTALACION ES
 # drush si standard --account-pass=admin --site-name=themerepo --locale=es -y
 
-# AUTOINSTALACIÓN EN
+# AUTOINSTALACION EN
 drush si druparcheky --account-pass=admin --site-name=themerepo -yvvv
 
 cmd.exe /C start http://themerepo:8888/user
