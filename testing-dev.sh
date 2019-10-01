8033c7ef91944a66948744bf977dcbcf35534e96
carcheky <carcheky@gmail.com>
2019-10-01 13:33:11 +0200

    modified testing file

--- testing-dev.sh
+++ testing-dev.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # AUTOINSTALACION ES
-drush si standard --account-pass=admin --site-name=themerepo --locale=es -yvvv
+drush si standard --account-pass=admin --site-name=druparcheky --locale=es -yvvv
 
 # AUTOINSTALACION EN
-# drush si standard --account-pass=admin --site-name=themerepo --locale=en -yvvv
+# drush si standard --account-pass=admin --site-name=druparcheky --locale=en -yvvv
 
-cmd.exe /C start http://themerepo:8888/user
+cmd.exe /C start http://druparcheky:8888/user
 # cmd.exe /C start $(dr uli)
