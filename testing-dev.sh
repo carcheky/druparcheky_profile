9cad52ccede34c2db46d732ac92ed2126b01efb9
carcheky <carcheky@gmail.com>
2019-10-02 16:18:45 +0200

    testingdev

--- testing-dev.sh
+++ testing-dev.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # AUTOINSTALACION ES
-drush si standard --account-pass=admin --site-name=druparcheky --locale=es -yvvv
+# drush si druparcheky --account-pass=admin --site-name=druparcheky --locale=es -yvvv
 
 # AUTOINSTALACION EN
-# drush si standard --account-pass=admin --site-name=druparcheky --locale=en -yvvv
+drush si druparcheky --account-pass=admin --site-name=druparcheky --locale=en -yvvv
 
 cmd.exe /C start http://druparcheky:8888/user
 # cmd.exe /C start $(dr uli)
